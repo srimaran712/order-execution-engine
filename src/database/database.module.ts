b6ee456791476318dcfa6c2f7e960ec3e9683ae9
Manimaran Srinivasan <srimaran712@gmail.com>
2025-12-19 08:48:47 +0530

    fixing the db connection on remote database host

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,7 +10,7 @@ import { Entities } from '../entities/initEntity.entity';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: 5433,
+        port: 5432,
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
